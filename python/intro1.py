6c62d6a08eeada7cae9bdaecd0fc4592ee5f339b
Rabimba Karanjai <rabimba@yahoo.com>
2014-01-20 16:30:04 -0600

    Initial Commit
    MLK Day Python Commit

---
+++ python/intro1.py
@@ -0,0 +1,17 @@
+print (type(3+7))
+print (9*8)
+print (type("this is a text"))
+
+
+a = 4
+b= a *9
+c= a - b
+
+if (a<0):
+    print ("a<o")
+    print (c)
+else:
+    print("a is not <0")
+    print(c)
+
+print("we are done")
---
+++ python/list.py
@@ -0,0 +1,4 @@
+a = [1,18,15,19]
+print (a)
+b=" I am "
+print (b)
