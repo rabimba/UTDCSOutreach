95d47d812bf574031fb5839926f7e5738d93db4c
Rabimba Karanjai <rabimba@yahoo.com>
2014-01-20 22:21:08 -0600

    Added web+Javascript

---
+++ web+js/dem/app/modules/baseModule/mainMenu/nls/resources.js
@@ -0,0 +1,15 @@
+define({
+    "root": {
+    	"home": "This is where you start UTDians",
+    	"productmodulea": "Warm-Up Demo",
+    	"productmoduleb": "Deep Diving",
+        "departments": "MVVM",
+        "clickcounter": "Click Counter",
+        "salesdashboard": "Complex Dashboard",
+        "employees": "List-Detail Panels",
+        "todo" : "TODO App"
+    },
+    
+    "sv" : true
+
+});
